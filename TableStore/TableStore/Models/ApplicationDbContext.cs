35046141d9dff327d84820349e8a568b6886ca41
buideptrai <itk9bui@gmail.com>
2026-05-19 22:32:33 +0700

    ddd

---
+++ TableStore/TableStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableStore.Models;
+
+namespace TableStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TableStore.Models.Category> Category { get; set; } = default!;
+    }
+}
